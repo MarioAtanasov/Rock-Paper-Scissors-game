741aa0a60f4fbf7b91d507643a540d0743801365
Mario Atanasov <124040978+MarioAtanasov@users.noreply.github.com>
2023-03-04 21:45:36 +0200

    rock paper scissor game

---
+++ rock paper scissor.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Welcome to Rock-Paper-Scissors!");
+Console.WriteLine("You will be playing against the computer. Good luck!");
+
+while (true)
+{
+    Console.WriteLine();
+    Console.Write("Enter your choice (rock, paper, or scissors): ");
+    string input = Console.ReadLine().ToLower();
+
+    if (input != "rock" && input != "paper" && input != "scissors")
+    {
+        Console.WriteLine("Invalid input. Please try again.");
+        continue;
+    }
+
+    Random random = new Random();
+    string[] options = { "rock", "paper", "scissors" };
+    string computerChoice = options[random.Next(options.Length)];
+
+    Console.WriteLine("The computer chose: " + computerChoice);
+
+    if (input == computerChoice)
+    {
+        Console.WriteLine("It's a draw!");
+    }
+    else if (input == "rock" && computerChoice == "scissors" ||
+             input == "paper" && computerChoice == "rock" ||
+             input == "scissors" && computerChoice == "paper")
+    {
+        Console.WriteLine("You win!");
+    }
+    else
+    {
+        Console.WriteLine("The computer wins!");
+    }
+
+    Console.WriteLine("Press any key to play again, or press Esc to exit.");
+    if (Console.ReadKey().Key == ConsoleKey.Escape)
+    {
+        break;
+    }
+}
